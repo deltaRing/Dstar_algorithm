bb5b65c996495ced812a7a2c8d96765a29c6fe57
e1ysium <43900243+deltaRing@users.noreply.github.com>
2023-07-26 21:57:55 +0800

    Update DStar.cpp

--- DStar.cpp
+++ DStar.cpp
@@ -123,7 +123,7 @@ void Dstar::InsertNode(State * state, double h_new){
     else if (state->t == _open_)
         state->k = min(h_new, state->k);
     else if (state->t == _close_)
-        state->k = min(h_new, state->k);
+        state->k = min(h_new, state->h);
     state->h = h_new;
     state->t = _open_;
     openList = InsertList(openList, state);
